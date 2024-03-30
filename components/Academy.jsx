b58f38785d751ffcd2237765f9a605ce96321ece
starbvuks <starbvuks@gmail.com>
2024-03-30 14:40:53 +0530

    fixed academy | xs mobile

--- components/Academy.jsx
+++ components/Academy.jsx
@@ -4,9 +4,8 @@ const Academy = () => {
   return (
     // <div className="w-full pb-20 pt-32 flex justify-center bg-gradient-to-b from-lime-800">
     <div className="rounded-t-[2.2em] -mt-8 w-full py-10 flex justify-center bg-black">
-      <div className="w-[80%] my-9 z-20 text-center rounded-3xl p-12 h-40 border-dashed border-white border-2 bg-emerald-900 bg-opacity-40 font-apercu text-white">
+      <div className="w-[80%] my-9 z-20 text-center flex flex-col justify-center gap-2 items-center rounded-3xl p-12 h-40 border-dashed border-white border-2 bg-emerald-900 bg-opacity-40 font-apercu text-white">
         <span>Launching Soon</span>
-        <br />
         <span className="font-bold text-3xl">Aleph Academy</span>
       </div>
     </div>
