b06756a8451a22931333605bdb910e242d8fe1e0
starbvuks <starbvuks@gmail.com>
2024-03-30 12:04:58 +0530

    more nav fixes | mobile

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full rounded-t-xl h-52 py-7 pl-8 pr-9 bg-black text-white grid grid-cols-2 justify-center items-center">
+    <div className="w-full rounded-xl h-40 py-7 pl-12 pr-9 bg-black text-white grid grid-cols-2 justify-center items-center">
       <div className="flex justify-center flex-col">
         <img
           src="/alephinnov-logo.png"
           alt="logo"
-          className="w-20 ml-6 object-cover"
+          className="w-20 mt-1 ml-6 object-cover"
         />
-        <span className="font-medium tracking-tighter font-apercu">
+        <span className="font-medium tracking-tighter font-apercu mt-1.5">
           Aleph Innovation
         </span>
       </div>
-      <div className="flex flex-col justify-end text-sm items-end text-right">
-        <span className="text-base">contact</span>
-        <div className="w-[60%] bg-white my-2 rounded-full opacity-20 h-0.5" />
-        <span>+91 95335 86416</span>
+      <div className="flex flex-col justify-end text-xs ml-6 text-left font-apercu">
+        <span className="text-sm font-light tracking-[0.16em] mb-4">CONTACT</span>
+        {/* <div className="w-[60%] bg-white my-2 rounded-full opacity-20 h-0.5" /> */}
+        <span className="mb-1">+91 95335 86416</span>
         <span>sarvag@startupgrind.com</span>
         {/* <hr className="mt-3 opacity-50"></hr> */}
         {/* <div>
